494972c5f970823d324fab51b48a086107288f4a
rsingh28 <rashmeet.singh28@gmail.com>
2018-01-28 02:02:48 -0500

    Complete working IBM Watson Integration based on implemented models

--- watson-integration/script.js
+++ watson-integration/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+var childProcess = require('child_process');
 var fs = require('fs');
 
 var personality_insights = new PersonalityInsightsV3({
@@ -9,15 +10,51 @@ var personality_insights = new PersonalityInsightsV3({
   version_date: '2016-10-19'
 });
 
-// Fire up a new terminal window and start the keylogger on it 
+// Running keylogger script to get the user input
+
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
 
 
+
+// Call to function to run script
+runScript('./keylogger/keylogger_testing.js', function (err) {
+    if (err) throw err;
+    console.log('finished running keylogger_testing.js');
+});
+
+// End of keylogger run and data is available to be manipulated
 
 /*
  * English example:
  *   'text' parameter contains the input text.
  */
 
+var file = fs.readFileSync("/var/log/keystroke.log", "utf8");
+console.log(file);
+
+// Sanity Check with test data
 personality_insights.profile(
   {
     content: 'The use of this video is allowed under failr dealing as it is for educational purposes. Education is one of the allowable uses for any work that is protected by the copyright laws in Canada. Since the group is presenting this work to a small groupand does not plan on any further distribution and revenue generation, the use is considered fair dealing. Since there is no cash or cash value of the prize, the purpose of this video is non-commercial and thus the group can use the video under user rights. The group must give proper credit to the original source of the video in order to be compliant of the copyright laws of Canadian Law.',
@@ -28,7 +65,8 @@ personality_insights.profile(
     if (err) {
       console.log('error:', err);
     } else {
-      console.log(JSON.stringify(response, null, 2));
+      //console.log(JSON.stringify(response, null, 2));
+      console.log("Success!");
     }
   }
 );
@@ -39,7 +77,7 @@ personality_insights.profile(
  */
 personality_insights
   .profile_csv({
-    content: 'The use of this video is allowed under failr dealing as it is for educational purposes. Education is one of the allowable uses for any work that is protected by the copyright laws in Canada. Since the group is presenting this work to a small groupand does not plan on any further distribution and revenue generation, the use is considered fair dealing. Since there is no cash or cash value of the prize, the purpose of this video is non-commercial and thus the group can use the video under user rights. The group must give proper credit to the original source of the video in order to be compliant of the copyright laws of Canadian Law.',
+    content: file,
     content_type: 'text/plain',
     csv_headers: true
   })
